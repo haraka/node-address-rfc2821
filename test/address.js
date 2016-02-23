574aa2bd69e91eeb17d43755c3fa8f75b2e82ef9
Matt Simerson <matt@tnpi.net>
2016-02-23 14:55:51 -0800

    remove .jshintrc, add .eslintrc

--- test/address.js
+++ test/address.js
@@ -11,11 +11,11 @@ function _check(address, user, host) {
 }
 
 describe('good addresses pass', function () {
-    
+
     it('<>', function () {
-        _check('<>', null, null);        
+        _check('<>', null, null);
     });
-    
+
     it('<postmaster>', function () {
         _check('<postmaster>', 'postmaster', null);
     });
@@ -28,32 +28,32 @@ describe('good addresses pass', function () {
         _check('<"musa_ibrah@caramail.comandrea.luger"@wifo.ac.at>',
             'musa_ibrah@caramail.comandrea.luger', 'wifo.ac.at');
     });
-    
+
     it('<foo bar@example.com>', function () {
         _check('<foo bar@example.com>', 'foo bar', 'example.com');
     });
-    
+
     it('foo@example.com', function () {
         _check('foo@example.com', 'foo', 'example.com');
     });
-    
+
     it('<foo@foo.x.example.com>', function () {
         _check('<foo@foo.x.example.com>', 'foo', 'foo.x.example.com');
     });
-    
+
     it('foo@foo.x.example.com', function () {
         _check('foo@foo.x.example.com', 'foo', 'foo.x.example.com');
     });
 });
 
 describe('bad addresses fail', function () {
-    
+
     it('<user@example.com#>', function () {
         assert.throws(function () {
             new Address('<user@example.com#>');
         });
     });
-    
+
     it('<user@example.com>.', function () {
         assert.throws(function () {
             new Address('<user@example.com.>');
@@ -62,7 +62,7 @@ describe('bad addresses fail', function () {
 });
 
 describe('compile_re', function () {
-    
+
     it('compiles with no exceptions', function () {
         var addr = require('../index');
         addr.compile_re();
@@ -74,7 +74,7 @@ describe('isNull', function () {
     it('positive <>', function () {
         assert.ok(new Address('<>').isNull());
     });
-    
+
     it('negative', function () {
         assert.ok(!new Address('<matt@example.com>').isNull());
     });
