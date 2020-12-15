d32ef6515d5960ebd51379e2499a30753636a3dc
Matt Simerson <matt@tnpi.net>
2020-12-14 17:59:32 -0800

    convert Address to es6 class

--- index.js
+++ index.js
@@ -6,37 +6,114 @@ const punycode = require('punycode');
 
 const qchar = /([^a-zA-Z0-9!#$%&\x27*+\x2D/=?^_`{|}~.\u00C0-\uFFFF])/g;
 
-function Address (user, host) {
-    if (typeof user === 'object' && user.original) {
-        // Assume constructing from JSON parse
-        for (const k in user) {
-            this[k] = user[k];
+class Address {
+    constructor (user, host) {
+        if (typeof user === 'object' && user.original) {
+            // Assume constructing from JSON parse
+            for (const k in user) {
+                this[k] = user[k];
+            }
+            return this;
         }
-        return this;
-    }
-    const match = /^<(.*)>$/.exec(user);
-    if (match) {
-        this.original = user;
-        this.parse(match[1]);
-    }
-    else if (!host) {
-        this.original = user;
-        this.parse(user);
-    }
-    else {
-        this.original = user + '@' + host;
-        this.user = user;
-        this.original_host = host;
+        const match = /^<(.*)>$/.exec(user);
+        if (match) {
+            this.original = user;
+            this.parse(match[1]);
+        }
+        else if (!host) {
+            this.original = user;
+            this.parse(user);
+        }
+        else {
+            this.original = `${user}@${host}`;
+            this.user = user;
+            this.original_host = host;
 
-        if (/[\u00C0-\uFFFF]/.test(host)) {
+            if (/[\u00C0-\uFFFF]/.test(host)) {
+                this.is_utf8 = true;
+                host = punycode.toASCII(host);
+            }
+
+            this.host = host;
+        }
+    }
+
+    parse (addr) {
+        // strip source route
+        addr = addr.replace(source_route_re, '');
+
+        // empty addr is ok
+        if (addr === '') {
+            this.user = '';
+            this.host = '';
+            return;
+        }
+
+        // bare postmaster is permissible: RFC-2821 (4.5.1)
+        if (addr.toLowerCase() === 'postmaster') {
+            this.user = 'postmaster';
+            this.host = '';
+            return;
+        }
+
+        let matches = user_host_re.exec(addr);
+
+        if (!matches) {
+            throw new Error(`Invalid domain in address: ${addr}`);
+        }
+
+        let localpart  = matches[1];
+        let domainpart = matches[2];
+        this.original_host = domainpart;
+
+        if (/[\u0100-\uFFFF]/.test(domainpart)) {
             this.is_utf8 = true;
-            host = punycode.toASCII(host);
+            domainpart = punycode.toASCII(domainpart);
         }
 
-        this.host = host;
+        this.host = domainpart.toLowerCase();
+
+        if (atoms_re.test(localpart)) {
+            // simple case, we are done
+            this.user = localpart;
+            return;
+        }
+        matches = qt_re.exec(localpart);
+        if (matches) {
+            localpart = matches[1];
+            this.user = localpart.replace(exports.text_expr, '$1');
+            return;
+        }
+        throw new Error(`Invalid local part in address: ${addr}`);
     }
 
+    isNull () {
+        return this.user ? 0 : 1;
+    }
+
+    format (use_punycode) {
+        if (this.isNull()) return '<>';
+
+        const user = this.user.replace(qchar, '\\$1');
+        if (user !== this.user) {
+            return `<"${user}"${this.original_host ? (`@${use_punycode ? this.host : this.original_host}`) : ''}>`;
+        }
+        return `<${this.address(null, use_punycode)}>`;
+    }
+
+    address (set, use_punycode) {
+        if (set) {
+            this.original = set;
+            this.parse(set);
+        }
+        return (this.user || '') + (this.original_host ? (`@${use_punycode ? this.host : this.original_host}`) : '');
+    }
+
+    toString () {
+        return this.format();
+    }
 }
+
 
 const idn_allowed = require('./_idn');
 
@@ -44,7 +121,7 @@ exports.atom_expr = /[a-zA-Z0-9!#%&*+=?^_`{|}~$\x27\x2D/\u00C0-\uFFFF]+/;
 exports.address_literal_expr =
   /(?:\[(?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|IPv6:[0-9A-Fa-f:.]+)\])/;
 // exports.subdomain_expr = /(?:[a-zA-Z0-9](?:[_\-a-zA-Z0-9]*[a-zA-Z0-9])?)/;
-exports.subdomain_expr = new RegExp('(?:' + idn_allowed.source + '(?:(?:[_-]|' + idn_allowed.source + ')*' + idn_allowed.source + ')?)');
+exports.subdomain_expr = new RegExp(`(?:${idn_allowed.source}(?:(?:[_-]|${idn_allowed.source})*${idn_allowed.source})?)`);
 
 // you can override this when loading and re-run compile_re()
 exports.domain_expr = undefined;
@@ -61,105 +138,23 @@ let qt_re;
 
 exports.compile_re = function () {
     domain_re = exports.domain_expr || new RegExp (
-        exports.subdomain_expr.source +
-            '(?:\\.' + exports.subdomain_expr.source + ')*'
+        `${exports.subdomain_expr.source}(?:\\.${exports.subdomain_expr.source})*`
     );
 
     if (!exports.domain_expr && exports.address_literal_expr) {
         // if address_literal_expr is set, add it in
-        domain_re = new RegExp('(?:' + exports.address_literal_expr.source +
-                               '|'   + domain_re.source + ')');
+        domain_re = new RegExp(`(?:${exports.address_literal_expr.source}|${domain_re.source})`);
     }
 
-    source_route_re = new RegExp('^@' + domain_re.source +
-                                 '(?:,@' + domain_re.source + ')*:');
+    source_route_re = new RegExp(`^@${domain_re.source }(?:,@${domain_re.source})*:`);
 
-    user_host_re = new RegExp('^(.*)@(' + domain_re.source + ')$');
+    user_host_re = new RegExp(`^(.*)@(${domain_re.source})$`);
 
-    atoms_re = new RegExp('^' + exports.atom_expr.source +
-                          '(\\.' + exports.atom_expr.source + ')*');
+    atoms_re = new RegExp(`^${exports.atom_expr.source}(\\.${exports.atom_expr.source})*`);
 
-    qt_re = new RegExp('^"((' + exports.qtext_expr.source +
-                       '|' + exports.text_expr.source + ')*)"$');
-};
+    qt_re = new RegExp(`^"((${exports.qtext_expr.source}|${exports.text_expr.source})*)"$`);
+}
 
 exports.compile_re();
-
-Address.prototype.parse = function (addr) {
-    // strip source route
-    addr = addr.replace(source_route_re, '');
-
-    // empty addr is ok
-    if (addr === '') {
-        this.user = '';
-        this.host = '';
-        return;
-    }
-
-    // bare postmaster is permissible: RFC-2821 (4.5.1)
-    if (addr.toLowerCase() === 'postmaster') {
-        this.user = 'postmaster';
-        this.host = '';
-        return;
-    }
-
-    let matches = user_host_re.exec(addr);
-
-    if (!matches) {
-        throw new Error('Invalid domain in address: ' + addr);
-    }
-
-    let localpart  = matches[1];
-    let domainpart = matches[2];
-    this.original_host = domainpart;
-
-    if (/[\u0100-\uFFFF]/.test(domainpart)) {
-        this.is_utf8 = true;
-        domainpart = punycode.toASCII(domainpart);
-    }
-
-    this.host = domainpart.toLowerCase();
-
-    if (atoms_re.test(localpart)) {
-        // simple case, we are done
-        this.user = localpart;
-        return;
-    }
-    matches = qt_re.exec(localpart);
-    if (matches) {
-        localpart = matches[1];
-        this.user = localpart.replace(exports.text_expr, '$1');
-        return;
-    }
-    throw new Error('Invalid local part in address: ' + addr);
-}
-
-Address.prototype.isNull = function () {
-    return this.user ? 0 : 1;
-}
-
-Address.prototype.format = function (use_punycode) {
-    if (this.isNull()) {
-        return '<>';
-    }
-
-    const user = this.user.replace(qchar, '\\$1');
-    if (user !== this.user) {
-        return '<"' + user + '"' + (this.original_host ? ('@' + (use_punycode ? this.host : this.original_host)) : '') + '>';
-    }
-    return '<' + this.address(null, use_punycode) + '>';
-}
-
-Address.prototype.address = function (set, use_punycode) {
-    if (set) {
-        this.original = set;
-        this.parse(set);
-    }
-    return (this.user || '') + (this.original_host ? ('@' + (use_punycode ? this.host : this.original_host)) : '');
-}
-
-Address.prototype.toString = function () {
-    return this.format();
-}
 
 exports.Address = Address;
